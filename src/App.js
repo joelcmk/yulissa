84bd6e25d6ab40b9c0efcc8e05e6efa054fef62a
joelcmk <57118300+joelcmk@users.noreply.github.com>
2022-08-08 12:25:58 -0500

    crate header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import About from './components/About';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <div className="header"></div>
+      <Header />
       <About />
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import '../App.css';
+function Header() {
+  return (
+    <div className="header">
+      <div className="yulissa font-bold text-left">
+        <p className="">Yulissa Chavez</p>
+      </div>
+      <div className="portfolio"></div>
+      <h1 className=" text-8xl font-bold">PORTFOLIO</h1>
+      <div className="portfolio"></div>
+    </div>
+  );
+}
+
+export default Header;
